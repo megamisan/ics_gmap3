af927c8493bde06baedbb6f8f07157a745b0db96
lstephan <lstephan@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-27 13:34:48 +0000

    maj js
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_gmap3/trunk@49147 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_gmap3/res/js/gmap3.js
+++ ics_gmap3/res/js/gmap3.js
@@ -37,54 +37,57 @@ ics.Map.prototype.addStaticData = function(data) {
 }; // Ajoute une liste de marqueur statique au stockage local
 
 ics.Map.prototype.createMarkersStatic_ = function(data) {
+	var icsmap = this;
 	var markersData = new Array();
 	for (index in data) {
 		markersData.push(data[index]);
 	}
-	
+	//alert('1');
 	jQuery('#' + this.gmap3).gmap3({
 		action: 'addMarkers',
 		markers: markersData,
 		marker: {
-			events:{
-				mouseover: function(marker, event, data){
-					jQuery(this).gmap3(
-					{
-						action:'clear', list:'overlay'
-					},
-					{
-						action:'addOverlay',
-						latLng: marker.getPosition(),
-						content:this.createWindowsInfo(data),
-						offset: {
-							x:-46,
-							y:-73
-						}
-					});
-				},
-				mouseout: function(){
-					jQuery(this).gmap3({action:'clear', list:'overlay'});
-				},
-				click: function(marker, event, data){
-					jQuery(this).gmap3({
-						action:'panTo', 
-						args:[marker.position]
-					});
-				}
-			}
+            options:{
+                icon:icsmap.getIcon(data),
+            },
+            events:{
+                mouseover: function(marker, event, data){
+                    var map = jQuery(this).gmap3('get'),
+                        infowindow = jQuery(this).gmap3({action:'get', name:'infowindow'});
+                    if (infowindow){
+                        infowindow.open(map, marker);
+                        infowindow.setContent(icsmap.createWindowsInfo(data));
+                    } else {
+                        jQuery(this).gmap3({action:'addinfowindow', anchor:marker, options:{content: icsmap.createWindowsInfo(data)}});
+                    }
+                },
+                mouseout: function(){
+                    var infowindow = $(this).gmap3({action:'get', name:'infowindow'});
+                    if (infowindow){
+                        infowindow.close();
+                    }
+                }
+            }
 		}
 	});
 }; // Exécution l'action d'ajout des marqueurs, à surcharger si on veux effectuer des actions supplémentaires sur les données des marqueurs.
 
 ics.Map.prototype.createWindowsInfo = function(row) {
-	content = '<div class="infobulle'+(data.access4disabled ? ' access4disabled' : '')+'">' +
-			'	<div class="bg"></div>' +
-			'	<div class="text"> ' + data.name  + ' ' + data.address  + '</div>' +
-			'</div>' +
-			'<div class="arrow"></div>';
-	alert(content);
+	var content = '';
+	for(var name in row) {
+		if(name != '' && name != 'undefined' && row[name] != 'undefined' && row[name] != '') {
+			content += '<p>' + row[name] + '</p>'; // à modifier DOM
+		}
+	}
 	return content;
 };
+
+ics.Map.prototype.getIcon = function(row) {
+	//alert(row);
+	//new google.maps.MarkerImage("http://jquery-ui.googlecode.com/svn-history/r3145/branches/labs/assets/theme/images/ui-icons_222222_256x240.png", new google.maps.Size(16, 16), new google.maps.Point((14.5*1), (14.5*10)))
+	return '';
+}
+
 
 //ics.Map.prototype.addDynamicData = function(url) { ... }; // Ajoute une url de données dynamique au stockage local
 //ics.Map.prototype.addBehaviourInit = function(func) { ... }; // Ajoute une fonction à appeler au stockage local
