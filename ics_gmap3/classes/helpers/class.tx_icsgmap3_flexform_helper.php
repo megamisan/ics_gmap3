81bc8d76603b6333567a697d53a22d1b8c0a3706
lstephan <lstephan@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-28 16:18:38 +0000

    Ajout flexform pour les champs windowsinfo
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_gmap3/trunk@49210 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_gmap3/classes/helpers/class.tx_icsgmap3_flexform_helper.php
+++ ics_gmap3/classes/helpers/class.tx_icsgmap3_flexform_helper.php
@@ -15,6 +15,20 @@ class tx_icsgmap3_flexform_helper {
 		$config['items'] = array_merge($config['items'],$optionList);
 		return $config;
 	}
+	
+	function renderFields($config) {
+		$optionList = array();
+		if(!empty($GLOBALS['TCA']['tt_address']['interface']['showRecordFieldList'])) {
+			$aFields = t3lib_div::trimExplode(',',$GLOBALS['TCA']['tt_address']['interface']['showRecordFieldList']);
+			foreach($aFields as $field) {
+				$fieldName = $GLOBALS['LANG']->sL($GLOBALS['TCA']['tt_address']['columns'][$field]['label']);
+				$optionList[] = array(0 => $fieldName, $field);
+			}
+		}
+		$config['items'] = array_merge($config['items'],$optionList);
+		return $config;
+	}
+	
 }
 
 ?>
