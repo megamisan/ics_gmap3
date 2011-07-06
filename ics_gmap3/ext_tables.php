ec9c8a0861a00e98ba26e6e91b11a8a86705a3ce
lstephan <lstephan@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-06 10:16:11 +0000

    Remove useless pi2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_gmap3/trunk@49455 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_gmap3/ext_tables.php
+++ ics_gmap3/ext_tables.php
@@ -24,20 +24,4 @@ if (TYPO3_MODE == 'BE') {
 	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_icsgmap3_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_icsgmap3_pi1_wizicon.php';
 }
 
-
-t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2']='layout,select_key,pages';
-
-
-t3lib_extMgm::addPlugin(array(
-	'LLL:EXT:ics_gmap3/locallang_db.xml:tt_content.list_type_pi2',
-	$_EXTKEY . '_pi2',
-	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
-),'list_type');
-
-
-
-if (TYPO3_MODE == 'BE') {
-	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_icsgmap3_pi2_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi2/class.tx_icsgmap3_pi2_wizicon.php';
-}
 ?>
