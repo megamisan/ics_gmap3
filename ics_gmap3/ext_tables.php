611d56dc94ec91f17ab11d23d3abac802820cb67
fredg <fredg@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-12-02 09:53:33 +0000

    Bug: Fixed case of "Classes" folder.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_gmap3/trunk@55246 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_gmap3/ext_tables.php
+++ ics_gmap3/ext_tables.php
@@ -18,7 +18,8 @@ t3lib_extMgm::addPlugin(array(
 $TCA["tt_content"]["types"]["list"]["subtypes_addlist"][$_EXTKEY."_pi1"]="pi_flexform";
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds_pi1.xml');
 
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getSingleFieldClass'][] = 'EXT:'.$_EXTKEY.'/classes/helpers/class.tx_icsgmap3_dynflex.php:&tx_icsgmap3_dynflex';
+//$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getSingleFieldClass'][] = 'EXT:'.$_EXTKEY.'/Classes/helpers/class.tx_icsgmap3_dynflex.php:&tx_icsgmap3_dynflex';
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getSingleFieldClass'][] = 'tx_icsgmap3_dynflex';
 
 if (TYPO3_MODE == 'BE') {
 	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_icsgmap3_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_icsgmap3_pi1_wizicon.php';
