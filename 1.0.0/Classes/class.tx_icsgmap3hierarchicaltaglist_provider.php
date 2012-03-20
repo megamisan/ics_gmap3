89e7bb5b49ee172e7c3de37e65a40ba22c7e72e1
mygoddess <mygoddess@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-20 15:19:07 +0000

    Creating tags for ics_gmap3 v1.0.0, ics_gmap3_hierarchical_taglist v1.0.0, ics_gmap3_multiple_tagslist v0.1.0, ics_gmap3_ttaddress v1.0.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_gmap3/tags/ics_gmap3_hierarchical_taglist@59408 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ 1.0.0/Classes/class.tx_icsgmap3hierarchicaltaglist_provider.php
@@ -0,0 +1,118 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 In Cité Solution <technique@in-cite.net>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+
+class tx_icsgmap3hierarchicaltaglist_provider implements tx_icsgmap3_iprovider {
+
+	var $extKey = 'ics_gmap3_hierarchical_taglist';
+	
+	function __construct() {
+		$this->uploadsPath = 'uploads/tx_icsgmap3hierarchicaltaglist/';
+		$this->flexform = file_get_contents(t3lib_div::getFileAbsFileName('EXT:' . $this->extKey . '/flexform_taglist_ds.xml'));
+	}
+	
+	function getStaticData($conf) {
+		return null;	
+	}
+	
+	function getDynamicDataUrl($conf) {
+		return null;
+	}
+	
+	function getBehaviourInitFunction($conf) {
+		$this->incJsFile(t3lib_extMgm::siteRelPath($this->extKey).'res/tx_icsgmap3hierarchicaltaglist.js', false, '_gmap3hierarchicaltaglist_provider');
+		
+		$jsCode = '';
+		$jsCode .= '
+			function (map) { 
+				var exclusivesTags = new Array();
+				var hiddenTags = new Array();
+				var defaultTags = new Array();
+				var lang = new Array();
+				
+				lang[\'select\'] = \'' . $GLOBALS['TSFE']->sL('LLL:EXT:ics_gmap3_hierarchical_taglist/locallang.xml:selectTags') . '\';
+				lang[\'unselect\'] = \'' . $GLOBALS['TSFE']->sL('LLL:EXT:ics_gmap3_hierarchical_taglist/locallang.xml:unselectTags') . '\';';
+		
+		$exclusivesTags = explode(',', $conf['exclusivesTags']);
+		if (is_array($exclusivesTags) && !empty($exclusivesTags)) {
+			foreach ($exclusivesTags as $tag) {
+				if ($tag) 
+					$jsCode .= '
+				exclusivesTags.push(\'' . addslashes($tag) . '\');';
+			}
+		}
+		$hiddenTags = explode(',', $conf['hiddenTags']);
+		if (is_array($hiddenTags) && !empty($hiddenTags)) {
+			foreach ($hiddenTags as $tag) {
+				if ($tag) 
+					$jsCode .= '
+				hiddenTags.push(\'' . addslashes($tag) . '\');';
+				
+			}
+		}
+		$defaultTags = explode(',', $conf['defaultTags']);
+		if (is_array($defaultTags) && !empty($defaultTags)) {
+			foreach ($defaultTags as $tag) {
+				if ($tag) 
+					$jsCode .= '
+				defaultTags.push(\'' . addslashes($tag) . '\');';
+			}
+		}
+		
+		$jsCode .= '
+				(new ics.HierarchicalTagList()).init(map, exclusivesTags, hiddenTags, defaultTags, ' . $conf['defaultMapEmpty'] . ', "' . $conf['separator'] . '", ' . $conf['checkOnParent'] . ', ' . $conf['viewLinkSelectAll'] . ', lang);
+			}
+		';
+		
+		return $jsCode;
+	}
+	
+	function getFlexform($conf) {
+		return $this->flexform;
+	}
+	
+	/**
+	* Function to insert Javascript at Ext. Runtime
+	*
+	* @param string $script Input the Script Name to insert JS
+	* @return
+	*/
+	function incJsFile($script,$jsCode = false, $suffix = '') {
+		if(!$jsCode)
+			$js = '<script src="'.$script.'" type="text/javascript"><!-- //--></script>';
+		else
+		{
+			$js .= '<script type="text/javascript">
+				'.$script.'
+			</script>';
+		}
+		$GLOBALS['TSFE']->additionalHeaderData[$this->extKey . $suffix . $this->cObj->data['uid']] .= $js;
+	}	
+}
+?>
---
+++ 1.0.0/ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ics_gmap3_hierarchical_taglist".
+#
+# Auto generated 20-03-2012 15:46
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Hierarchical tag list',
+	'description' => 'Hierarchical tag list for gmap3 (BEHAVIOUR_ADD)',
+	'category' => 'be',
+	'author' => 'In Cite Solution',
+	'author_email' => 'technique@in-cite.net',
+	'shy' => '',
+	'dependencies' => 'ics_gmap3',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'ics_gmap3' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"9a19";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"fcd9";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"3adc";s:14:"ext_tables.php";s:4:"8b61";s:23:"flexform_taglist_ds.xml";s:4:"39b6";s:13:"locallang.xml";s:4:"7c22";s:57:"Classes/class.tx_icsgmap3hierarchicaltaglist_provider.php";s:4:"4c13";s:19:"doc/wizard_form.dat";s:4:"6c6b";s:20:"doc/wizard_form.html";s:4:"fff1";s:37:"res/tx_icsgmap3hierarchicaltaglist.js";s:4:"6078";s:40:"static/hierachical_taglist/constants.txt";s:4:"d41d";s:36:"static/hierachical_taglist/setup.txt";s:4:"d41d";}',
+	'suggests' => array(
+	),
+);
+
+?>
