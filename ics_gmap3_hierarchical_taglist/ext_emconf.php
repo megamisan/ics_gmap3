2c40e0190050d5bc93efafdb3437fbb25255ebcc
emilieprudhomme <emilieprudhomme@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-20 14:55:41 +0000

    - Change extensions versions: ics_gmap3 => 1.0.0 ics_gmap3_hierarchical_taglist => 1.0.0 ics_gmap3_ttaddress => 1.0.0 ics_gmap3_multiple_tagslist => 0.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_gmap3/trunk@59400 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_gmap3_hierarchical_taglist/ext_emconf.php
+++ ics_gmap3_hierarchical_taglist/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ics_gmap3_hierarchical_taglist".
 #
-# Auto generated 17-01-2012 10:15
+# Auto generated 20-03-2012 15:46
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -14,14 +14,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Hierarchical tag list',
 	'description' => 'Hierarchical tag list for gmap3 (BEHAVIOUR_ADD)',
 	'category' => 'be',
-	'author' => 'GOYER Frederic',
-	'author_email' => 'fgoyer@in-cite.net',
+	'author' => 'In Cite Solution',
+	'author_email' => 'technique@in-cite.net',
 	'shy' => '',
 	'dependencies' => 'ics_gmap3',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'ics_gmap3' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"9a19";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"fcd9";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"3adc";s:14:"ext_tables.php";s:4:"8b61";s:23:"flexform_taglist_ds.xml";s:4:"992d";s:13:"locallang.xml";s:4:"8fc3";s:57:"Classes/class.tx_icsgmap3hierarchicaltaglist_provider.php";s:4:"c9b7";s:19:"doc/wizard_form.dat";s:4:"6c6b";s:20:"doc/wizard_form.html";s:4:"fff1";s:46:"res/tx_icsgmap3hierarchicaltaglist-20111205.js";s:4:"64d8";s:37:"res/tx_icsgmap3hierarchicaltaglist.js";s:4:"f055";s:40:"static/hierachical_taglist/constants.txt";s:4:"d41d";s:36:"static/hierachical_taglist/setup.txt";s:4:"d41d";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"9a19";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"fcd9";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"3adc";s:14:"ext_tables.php";s:4:"8b61";s:23:"flexform_taglist_ds.xml";s:4:"39b6";s:13:"locallang.xml";s:4:"7c22";s:57:"Classes/class.tx_icsgmap3hierarchicaltaglist_provider.php";s:4:"4c13";s:19:"doc/wizard_form.dat";s:4:"6c6b";s:20:"doc/wizard_form.html";s:4:"fff1";s:37:"res/tx_icsgmap3hierarchicaltaglist.js";s:4:"6078";s:40:"static/hierachical_taglist/constants.txt";s:4:"d41d";s:36:"static/hierachical_taglist/setup.txt";s:4:"d41d";}',
 	'suggests' => array(
 	),
 );
--- ics_gmap3_multiple_tagslist/ext_emconf.php
+++ ics_gmap3_multiple_tagslist/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ics_gmap3_multiple_tagslist".
 #
-# Auto generated 20-03-2012 13:53
+# Auto generated 20-03-2012 15:48
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'ics_gmap3' => '',
@@ -39,7 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"2106";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"a797";s:19:"doc/wizard_form.dat";s:4:"7566";s:20:"doc/wizard_form.html";s:4:"9c40";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"d41d";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"2106";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"2cff";s:14:"ext_tables.php";s:4:"a797";s:33:"flexform_ds_multiple_tagslist.xml";s:4:"775d";s:13:"locallang.xml";s:4:"8cf5";s:53:"Classes/class.user_icsgmap3taglist_additionalFlex.php";s:4:"85f5";s:49:"Classes/class.ux_tx_icsgmap3_taglist_provider.php";s:4:"4afc";s:19:"doc/wizard_form.dat";s:4:"7566";s:20:"doc/wizard_form.html";s:4:"9c40";s:29:"res/gmap3_multiple_taglist.js";s:4:"8691";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"d41d";}',
+	'suggests' => array(
+	),
 );
 
 ?>
