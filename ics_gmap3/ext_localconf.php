c65ea09b36b246c0a705d20f7568e90b12703189
emilieprudhomme <emilieprudhomme@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-23 09:20:28 +0000

    Fix: Remove pi2 in ext_localconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_gmap3/trunk@59610 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_gmap3/ext_localconf.php
+++ ics_gmap3/ext_localconf.php
@@ -5,8 +5,6 @@ if (!defined('TYPO3_MODE')) {
 
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_icsgmap3_pi1.php', '_pi1', 'list_type', 0);
 
-t3lib_extMgm::addPItoST43($_EXTKEY, 'pi2/class.tx_icsgmap3_pi2.php', '_pi2', 'list_type', 0);
-
 tx_icsgmap3_provider_manager::subscribe(tx_icsgmap3_provider_manager::BEHAVIOUR_ADD, 'tx_icsgmap3_taglist_provider', 'LLL:EXT:ics_gmap3/locallang.xml:provider.tagList');
 
 //$TYPO3_CONF_VARS['BE']['PROVIDERS']['tceFormsProviderList'] = t3lib_extMgm::extPath('ics_gmap3').'lib/class.tx_icsgmap3_providerList.php:tx_icsgmap3_providerList->displayProviders';
