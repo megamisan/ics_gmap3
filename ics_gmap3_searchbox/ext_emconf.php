a1c8f3eb8a4d2112190191bc48370d5be06a2397
emilieprudhomme <emilieprudhomme@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-16 12:25:28 +0000

    - Change extensions versions: ics_gmap3 => 1.1.0 ics_gmap3_datalist => 0.1.0 ics_gmap3_multiple_taglist => 0.2.0 ics_gmap3_ttaddress => 1.1.0 ics_gmap3_searchbox => 0.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_gmap3/trunk@60773 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_gmap3_searchbox/ext_emconf.php
+++ ics_gmap3_searchbox/ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "ics_gmap3_datalist".
+# Extension Manager/Repository config file for ext "ics_gmap3_searchbox".
 #
-# Auto generated 17-01-2012 10:54
+# Auto generated 16-04-2012 14:21
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'ics_gmap3' => '',
@@ -39,7 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"c119";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"c548";s:20:"doc/wizard_form.html";s:4:"1858";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"9e38";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"0efc";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"95cf";s:25:"flexform_searchbox_ds.xml";s:4:"5487";s:13:"locallang.xml";s:4:"7071";s:47:"Classes/class.tx_icsgmap3searchbox_provider.php";s:4:"fbff";s:27:"res/tx_icsgmap3searchbox.js";s:4:"f5d1";}',
+	'suggests' => array(
+	),
 );
 
 ?>
--- ics_gmap3_ttaddress/ext_emconf.php
+++ ics_gmap3_ttaddress/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ics_gmap3_ttaddress".
 #
-# Auto generated 20-03-2012 15:47
+# Auto generated 16-04-2012 14:21
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'tt_address' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"4b55";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"3638";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"7dd6";s:14:"ext_tables.php";s:4:"8f08";s:14:"ext_tables.sql";s:4:"28d9";s:15:"flexform_ds.xml";s:4:"e94c";s:13:"locallang.xml";s:4:"096f";s:16:"locallang_db.xml";s:4:"af3f";s:47:"classes/class.tx_icsgmap3ttaddress_provider.php";s:4:"6fb2";s:64:"classes/helpers/class.tx_icsgmap3_tt_address_flexform_helper.php";s:4:"438f";s:19:"doc/wizard_form.dat";s:4:"256d";s:20:"doc/wizard_form.html";s:4:"2012";s:19:"res/createMarker.js";s:4:"dc3a";s:34:"static/addresssource/constants.txt";s:4:"68e7";s:30:"static/addresssource/setup.txt";s:4:"c468";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"2401";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"3638";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"7dd6";s:14:"ext_tables.php";s:4:"b847";s:14:"ext_tables.sql";s:4:"28d9";s:15:"flexform_ds.xml";s:4:"e94c";s:13:"locallang.xml";s:4:"096f";s:16:"locallang_db.xml";s:4:"af3f";s:47:"classes/class.tx_icsgmap3ttaddress_provider.php";s:4:"b972";s:64:"classes/helpers/class.tx_icsgmap3_tt_address_flexform_helper.php";s:4:"438f";s:19:"doc/wizard_form.dat";s:4:"256d";s:20:"doc/wizard_form.html";s:4:"2012";s:19:"res/createMarker.js";s:4:"dc3a";s:34:"static/addresssource/constants.txt";s:4:"68e7";s:30:"static/addresssource/setup.txt";s:4:"c468";}',
 	'suggests' => array(
 	),
 );
